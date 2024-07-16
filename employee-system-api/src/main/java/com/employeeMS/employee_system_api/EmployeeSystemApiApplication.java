6f6db0ed52809e6b812043600fd84483cfe349d3
Mishan20 <ishansenanayaka1@gmail.com>
2024-07-16 17:19:46 +0530

    First commit

---
+++ employee-system-api/src/main/java/com/employeeMS/employee_system_api/EmployeeSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.employeeMS.employee_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSystemApiApplication.class, args);
+	}
+
+}
---
+++ employee-system-api/src/test/java/com/employeeMS/employee_system_api/EmployeeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeeMS.employee_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

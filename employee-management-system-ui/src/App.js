13056a205db41f1d273c0edecd76545eae23fc2d
Mishan20 <ishansenanayaka1@gmail.com>
2024-07-20 23:34:24 +0530

    add new route

--- employee-management-system-ui/src/App.js
+++ employee-management-system-ui/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import AddEmployee from "./components/AddEmployee";
 import Navbar from "./components/Navbar";
 import EmployeeList from "./components/EmployeeList";
+import UpdateEmployee from "./components/UpdateEmployee";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<EmployeeList />} />
           <Route path="/employeeList" element={<EmployeeList />} />
           <Route path="/addEmployee" element={<AddEmployee />} />
+          <Route path="/editEmployee/:id" element={<UpdateEmployee />} />
         </Routes>
       </BrowserRouter>
     </>

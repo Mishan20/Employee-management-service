aba1e067662ac8dfe202b37691cb6fe12b6a3f20
Mishan20 <ishansenanayaka1@gmail.com>
2024-07-20 23:32:52 +0530

    create edit and delete employee api

--- employee-system-api/src/main/java/com/employeeMS/employee_system_api/controller/EmployeeController.java
+++ employee-system-api/src/main/java/com/employeeMS/employee_system_api/controller/EmployeeController.java
@@ -4,9 +4,12 @@ package com.employeeMS.employee_system_api.controller;
 import com.employeeMS.employee_system_api.model.Employee;
 import com.employeeMS.employee_system_api.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -27,5 +30,28 @@ public class EmployeeController  {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Long id){
+        boolean deleted = false;
+        deleted = employeeService.deleteEmployee(id);
+        Map<String,Boolean> response = new HashMap<>();
+        response.put("deleted",deleted);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = null;
+        employee = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,
+                                                   @RequestBody Employee employee) {
+        employee = employeeService.updateEmployee(id, employee);
+        return ResponseEntity.ok(employee);
     }
 }
--- employee-system-api/src/main/java/com/employeeMS/employee_system_api/services/EmployeeService.java
+++ employee-system-api/src/main/java/com/employeeMS/employee_system_api/services/EmployeeService.java
@@ -8,4 +8,10 @@ public interface EmployeeService {
     Employee createEmployee(Employee employee);
 
     List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
 }
